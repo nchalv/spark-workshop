4c007e6bd5314ab085d8149469615c375377f83d
Nikos Chalvantzis <nchalv@cslab.ece.ntua.gr>
2024-08-29 13:29:37 +0300

    Updates to code.

--- code/00_csv_to_parquet.py
+++ code/00_csv_to_parquet.py
@@ -5,7 +5,7 @@ from pyspark.sql.types import IntegerType, DoubleType
 spark = SparkSession.builder \
     .appName("csv to parquet - optional") \
     .getOrCreate()
-
+#Set the path to the data source
 path = "hdfs://controller:54310/user/ubuntu/data/la_crime_data/CrimeData_10-19.csv"
 df = spark.read.option("header", "true").csv(path)
 date_format = "MM/dd/yyyy hh:mm:ss a"
@@ -14,6 +14,6 @@ df = df.withColumn("Date Rptd", to_date("Date Rptd", date_format)) \
     .withColumn("LAT", col("LAT").cast(DoubleType())) \
     .withColumn("LON", col("LON").cast(DoubleType())) \
     .withColumn("Vict Age", col("Vict Age").cast(IntegerType()))
-# Write DataFrame to Parquet
+# Write DataFrame to Parquet - in contrast to csv, it can also store data types
 df.write \
-    .parquet("hdfs://controller:54310/home/ubuntu/data/la_crime_data/parquet")
+    .parquet("hdfs://controller:54310/user/ubuntu/data/la_crime_data/parquet")
